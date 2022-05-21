b559c7fee3e1abeb75dad4604baca6da31a75fa6
AndrewY7321 <62182495+PythonCodeingKing@users.noreply.github.com>
2022-05-21 13:15:35 -0600

    Add files via upload

---
+++ legendOfDark/dialogLoader.py
@@ -0,0 +1,5 @@
+def load_dialog(dialogID):
+	if dialogID == 1:
+		return "Welcome to the store!\nI am selling health for 1 coin.\nPress E To Buy, R To close"
+	if dialogID == 2:
+		return "Hello, welcome to my store.\nUnfortunately, I am out of stock.\nPlease Come back later.\nPress R or E to Close"
