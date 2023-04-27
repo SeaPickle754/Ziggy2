7dc2c76dc91eb1621e1fa14e52aa9ccef1305050
SeaPickle <uraniumpjs@gmail.com>
2023-04-26 18:20:34 -0600

    Tuning and fixing messages

--- dialogLoader.py
+++ dialogLoader.py
@@ -1,9 +1,9 @@
 def load_dialog(dialogID):
 	if dialogID == 1:
-		return "Welcome to the store!\nI am selling health for 1 coin.\nPress R To Buy, E To close"
+		return "Welcome to the store!\nI am selling health for 1 coin.\nPress E To Buy, R To close"
 	if dialogID == 0:
 		return "Hello, welcome to my store.\nUnfortunately, I am out of stock.\nPlease Come back later.\nPress R or E to Close"
 	if dialogID == 2:
-		return "^ Route to much pain https://tinyurl.com/ziggy2site"
+		return "^ Route to much pain \n https://tinyurl.com/ziggy2site"
 	if dialogID == 3:
 		return "*passive agressive*\n Sorry, you are OUT of coins. \n Get a job like me and earn some! \n some people are just lucky... \n They find coins on the ground... ANYWAYS!\n Press R or E to close."
